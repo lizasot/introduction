27097511c35aa6cb5e6a9e2d60fb0aef8a211899
Krist <Kri2t@yandex.ru>
2022-04-29 00:00:50 +0300

    Добавьте файлы проекта.

---
+++ lesson1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ваше имя: ");
+            string username = Console.ReadLine();
+            Console.WriteLine($"Привет, {username}, сегодня {DateTime.Now}");
+        }
+    }
+}
