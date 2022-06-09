9b820de745e65c49174374409b34b653197b3983
Krist <Kri2t@yandex.ru>
2022-06-09 18:01:22 +0300

    Выполнена задача 8 урока

---
+++ lesson8.Description/OutputHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson8.MyUtils
+{
+    public class OutputHelper
+    {
+        public static void PrintInfo(int numb, string fio)
+        {
+            Console.WriteLine("║ Урок " + numb + ".");
+            Console.WriteLine("║ Студент: " + fio);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ lesson8/Program.cs
@@ -0,0 +1,52 @@
+﻿using lesson8.MyUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputHelper.PrintInfo(8, "Сотина Елизавета");
+            Console.WriteLine(Properties.Settings.Default.GreetingMsg);
+            Console.WriteLine();
+            if (Properties.Settings.Default.Name == "UserName")
+            {
+                Console.Write("Укажите ваше имя: ");
+                Properties.Settings.Default.Name = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Console.WriteLine("Ваше имя: " + Properties.Settings.Default.Name);
+            }
+
+            if (Properties.Settings.Default.Age == 0)
+            {
+                Console.Write("Укажите ваш возраст: ");
+                Properties.Settings.Default.Age = int.Parse(Console.ReadLine());
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Console.WriteLine("Ваш возраст: " + Properties.Settings.Default.Age);
+            }
+
+            if (Properties.Settings.Default.Employment == "None")
+            {
+                Console.Write("Укажите ваш род деятельности: ");
+                Properties.Settings.Default.Employment = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Console.WriteLine("Ваш род деятельности: " + Properties.Settings.Default.Employment);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
